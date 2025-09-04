86eac2d207bf419c9baafc3bb9ffd762f15d79d6
Your-Personal-Name <abhishekolympics@gmail.com>
2025-09-04 23:49:41 +0530

    refactor: replace useState with useReducer for state management in App and Counter components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 import { CountContext } from './contexts/CountContext'
 import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const initialState = { count: 0, theme: 'dark' };
 
+  function reducer(state, action) {
+    switch(action.type)
+    {
+      case 'increment':
+        return {...state, count:state.count + 1};
+      case 'decrement':
+        return {...state, count:state.count - 1};
+      case 'Dark':
+        return { ...state, theme: 'Dark'}
+      case 'Light':
+        return { ...state, theme: 'Light'}
+      case 'reset': 
+        return { ...state, theme: 'Dark', count: 0 }
+      default: return state;
+    }
+  }
+
+  const [ state, dispatch ] = useReducer(reducer, initialState );
   return (
-    <CountContext.Provider value={{ count, setCount }}>
+    <CountContext.Provider value={{ state, dispatch }}>
      <Counter/>
     </CountContext.Provider>
   )
--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -1,24 +1,30 @@
 import { useContext, useEffect } from "react";
 import { CountContext } from "../contexts/CountContext";
 
-export default function counter() {
+export default function Counter() {
 
-    const { count, setCount } = useContext(CountContext);
+    const { state, dispatch } = useContext(CountContext);
 
-    function handleCounter() {
-        setCount(count + 1);
-    }
+    // function handleCounter() {
+    //     setCount(count + 1);
+    // }
     
     useEffect(() => {
-        console.log("re-rendered ", count);
-    },[count])
+        console.log("re-rendered ");
+    },[state.count, state.theme])
     
     return (
         <>
              <h1>Counter App</h1>
-            <h2>{count}</h2>
+            <h2>{state.count}</h2>
+            <h2>{state.theme}</h2>
 
-            <button onClick={handleCounter}>Increase counter</button>
+            <button onClick={() => dispatch({type: 'increment'})}>Increase counter</button>
+            <button onClick={() => dispatch({type: 'decrement'})}>Decrease counter</button>
+            <button onClick={() => dispatch({type: 'Light'})}>Light Theme</button>
+            <button onClick={() => dispatch({type: 'Dark'})}>Dark Theme</button>
+            <button onClick={() => dispatch({type: 'reset'})}>Dark Theme</button>
+
             {/* <button onClick={(() => setCount(count + 1))}>Increase counter</button> */}
             {/* <button onClick={setCount(count + 1)}>Increase counter</button> */}
         </>
